1f73121731a30d48d89c1342fa28e1043a50b26f
Martin Callejas <amcallejasherrera@gmail.com>
2020-05-07 15:41:24 -0400

    Delete useless methods

--- martin_callejas/task1_tic-tac-toe/src/jalasoft/training/Spot.java
+++ martin_callejas/task1_tic-tac-toe/src/jalasoft/training/Spot.java
@@ -17,20 +17,4 @@ public class Spot {
     public void setValue(char value) {
         this.value = value;
     }
-
-    public int getPosX() {
-        return posX;
-    }
-
-    public void setPosX(int posX) {
-        this.posX = posX;
-    }
-
-    public int getPosY() {
-        return posY;
-    }
-
-    public void setPosY(int posY) {
-        this.posY = posY;
-    }
 }
