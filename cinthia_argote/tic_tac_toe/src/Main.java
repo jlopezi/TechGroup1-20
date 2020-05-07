3df365a0cf5f068a6516f04f9d246f70642dd96b
ximena subieta <ximegasub@gmail.com>
2020-05-06 23:46:33 -0400

    Task Tic Tac Toe

--- cinthia_argote/tic_tac_toe/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-  public static void main(String [] args){
-    System.out.println("Game Tic Tac Toe");
-    TicTacToe ticTacToe = new TicTacToe();
-    ticTacToe.playGame();
-   }
-}
