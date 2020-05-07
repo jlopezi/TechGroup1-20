401a82d5bdc2bed900ec13b322725fe1bb82a0fe
daniel vargas sosa <daniel.vargas.sosa@gmail.com>
2020-05-07 15:46:06 -0400

    Fixing comments

--- DanielVargas/tic_tac_toe/src/com/company/Main.java
+++ DanielVargas/tic_tac_toe/src/com/company/Main.java
@@ -1,16 +1,17 @@
-    package com.company;
+package com.company;
+
 import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
-        Scanner keyboard=new Scanner(System.in);
-        String player1,player2;
-        System.out.println("Type tha player1's name " );
-        player1=keyboard.next();
-        System.out.println("Type tha player2's name " );
-        player2=keyboard.next();
-	    Tictactoe newgame= new Tictactoe(player1,player2);
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String player1, player2;
+        System.out.println("Type the player1's name ");
+        player1 = keyboard.next();
+        System.out.println("Type the player2's name ");
+        player2 = keyboard.next();
+        TicTacToe newgame = new TicTacToe(player1, player2);
         newgame.game_on();
     }
 }
