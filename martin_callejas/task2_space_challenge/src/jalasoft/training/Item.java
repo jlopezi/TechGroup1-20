5a88c81a38a9ca05f07c3c58d583562968cd03dd
Martin Callejas <amcallejasherrera@gmail.com>
2020-05-13 04:50:25 -0400

    Add implementation

---
+++ martin_callejas/task2_space_challenge/src/jalasoft/training/Item.java
@@ -0,0 +1,15 @@
+package jalasoft.training;
+
+public class Item {
+    private String name;
+    private int weight;
+
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
--- martin_callejas/task2_space_challenge/src/jalasoft/training/Main.java
+++ martin_callejas/task2_space_challenge/src/jalasoft/training/Main.java
@@ -3,6 +3,15 @@ package jalasoft.training;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Simulation mySimulation = new Simulation();
+
+        int fleetU1budget = mySimulation.runSimulation(mySimulation.loadU1(mySimulation.loadItems("phase-1.txt")))
+                            + mySimulation.runSimulation(mySimulation.loadU1(mySimulation.loadItems("phase-2.txt")));
+        System.out.println(String.format("U1 Rockets fleet total budget: $%d Million", fleetU1budget));
+
+        int fleetU2budget = mySimulation.runSimulation(mySimulation.loadU2(mySimulation.loadItems("phase-1.txt")))
+                + mySimulation.runSimulation(mySimulation.loadU2(mySimulation.loadItems("phase-2.txt")));
+        System.out.println(String.format("U2 Rockets fleet total budget: $%d Million", fleetU2budget));
+
     }
 }
